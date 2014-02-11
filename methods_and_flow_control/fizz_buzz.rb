9bc0a1a7d374dc976d1b8199c98363fa2e542656
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-02-11 13:21:46 +0000

    added fizz_buzz.rb

---
+++ methods_and_flow_control/fizz_buzz.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+def fizzbuzz i  # i is number to convert to fizzbuzz text
+  fizz_buzz = 0 == i % 3 ? "fizz" : ""   # "fizz" if divisible by 3
+  fizz_buzz += "buzz" if 0 == i % 5      # append "buzz" if diviible by 5
+  fizz_buzz = i.to_s if fizz_buzz.empty?  # otherwise just the number (as a string)
+  return fizz_buzz
+end
+
+(1..100).each {|i| puts fizzbuzz(i)}

12586d0795a312449cb2f3e2537a0995daabe4b7
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-02-13 19:51:37 +0000

    fizz buzz with 755 permissions

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -3,7 +3,7 @@
 def fizzbuzz i  # i is number to convert to fizzbuzz text
   fizz_buzz = (0 == i % 3) ? "Fizz" : ""   # "fizz" if divisible by 3
   fizz_buzz += "Buzz" if (0 == i % 5)      # append "buzz" if diviible by 5
-  fizz_buzz = i.to_s if fizz_buzz.empty?  # otherwise just the number (as a string)
+  fizz_buzz = i if fizz_buzz.empty?  # otherwise just the number (as a string)
   return fizz_buzz
 end
 

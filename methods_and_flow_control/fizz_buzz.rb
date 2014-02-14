bdbde352076a218f96ace853b1bd03e3fb3c5965
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-02-14 06:24:01 +0000

    fix fizz_buzz to be more readable code.

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-def fizzbuzz i  # i is number to convert to fizzbuzz text
-  fizz_buzz = (0 == i % 3) ? "Fizz" : ""   # "fizz" if divisible by 3
-  fizz_buzz += "Buzz" if (0 == i % 5)      # append "buzz" if diviible by 5
-  fizz_buzz = i if fizz_buzz.empty?  # otherwise just the number (as a string)
+def fizzbuzz(number)  # number to convert to fizzbuzz text
+  fizz_buzz = (0 == number % 3) ? "Fizz" : ""   # "fizz" if divisible by 3
+  fizz_buzz += "Buzz" if (0 == number % 5)      # append "buzz" if divisible by 5
+  fizz_buzz = number.to_s if fizz_buzz.empty?  # otherwise just the number (as a string)
   return fizz_buzz
 end
 

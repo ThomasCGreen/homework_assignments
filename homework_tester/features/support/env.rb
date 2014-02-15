9ed2e11eb4f8894ef0881195e1eab92c89aa6053
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-02-15 22:12:08 +0000

    added changes to env.rb

--- homework_tester/features/support/env.rb
+++ homework_tester/features/support/env.rb
@@ -2,5 +2,5 @@ $: << File.expand_path("../../lib", File.dirname(__FILE__))
 require 'aruba/cucumber'
 
 Before do
-  @dirs = File.join(File.dirname(__FILE__), "../../../homework_assignments")
+  @dirs = File.join(File.dirname(__FILE__), "../..")
 end

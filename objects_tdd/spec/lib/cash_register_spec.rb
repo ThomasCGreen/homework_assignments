7f62e157ba6e6b75c015d087f270992321e9d7db
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-02-21 18:25:45 +0000

    added some context blocks to the cash register tests

--- objects_tdd/spec/lib/cash_register_spec.rb
+++ objects_tdd/spec/lib/cash_register_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require File.join(File.dirname(__FILE__),'../../lib/cash_register')
+require File.join(File.dirname(__FILE__), '../../lib/cash_register')
 
 describe CashRegister do
   describe "total" do
@@ -27,38 +27,44 @@ describe CashRegister do
   end
 
   describe "pay(amount)" do
-    it "should return 'Your new total is $4.00' if pay(5.00)" do
-      expect(subject.purchase(3.78)).to eq(3.78)
-      expect(subject.total).to eq(3.78)
-      expect(subject.purchase(5.22)).to eq(9.00)
-      expect(subject.pay(5.00)).to eq("Your new total is $4.00.")
+    context "Paying less than total" do
+      it "should return 'Your new total is $4.00' if pay(5.00)" do
+        expect(subject.purchase(3.78)).to eq(3.78)
+        expect(subject.total).to eq(3.78)
+        expect(subject.purchase(5.22)).to eq(9.00)
+        expect(subject.pay(5.00)).to eq("Your new total is $4.00.")
+      end
+
+      it "should return '4.00' if you now ask for total" do
+        expect(subject.purchase(3.78)).to eq(3.78)
+        expect(subject.total).to eq(3.78)
+        expect(subject.purchase(5.22)).to eq(9.00)
+        expect(subject.pay(5.00)).to eq("Your new total is $4.00.")
+        expect(subject.total).to eq(4.00)
+      end
+
     end
 
-    it "should return '4.00' if you now ask for total" do
-      expect(subject.purchase(3.78)).to eq(3.78)
-      expect(subject.total).to eq(3.78)
-      expect(subject.purchase(5.22)).to eq(9.00)
-      expect(subject.pay(5.00)).to eq("Your new total is $4.00.")
-      expect(subject.total).to eq(4.00)
-    end
+    context "Paying more than total" do
 
-    it "should return 'Your change is $1.00' if pay(5.00) again" do
-      expect(subject.purchase(3.78)).to eq(3.78)
-      expect(subject.total).to eq(3.78)
-      expect(subject.purchase(5.22)).to eq(9.00)
-      expect(subject.pay(5.00)).to eq("Your new total is $4.00.")
-      expect(subject.total).to eq(4.00)
-      expect(subject.pay(5.00)).to eq("Your change is $1.00.")
-    end
+      it "should return 'Your change is $1.00' if pay(5.00) again" do
+        expect(subject.purchase(3.78)).to eq(3.78)
+        expect(subject.total).to eq(3.78)
+        expect(subject.purchase(5.22)).to eq(9.00)
+        expect(subject.pay(5.00)).to eq("Your new total is $4.00.")
+        expect(subject.total).to eq(4.00)
+        expect(subject.pay(5.00)).to eq("Your change is $1.00.")
+      end
 
-    it "should return '0.00' when we check total again" do
-      expect(subject.purchase(3.78)).to eq(3.78)
-      expect(subject.total).to eq(3.78)
-      expect(subject.purchase(5.22)).to eq(9.00)
-      expect(subject.pay(5.00)).to eq("Your new total is $4.00.")
-      expect(subject.total).to eq(4.00)
-      expect(subject.pay(5.00)).to eq("Your change is $1.00.")
-      expect(subject.total).to eq(0.00)
+      it "should return '0.00' when we check total again" do
+        expect(subject.purchase(3.78)).to eq(3.78)
+        expect(subject.total).to eq(3.78)
+        expect(subject.purchase(5.22)).to eq(9.00)
+        expect(subject.pay(5.00)).to eq("Your new total is $4.00.")
+        expect(subject.total).to eq(4.00)
+        expect(subject.pay(5.00)).to eq("Your change is $1.00.")
+        expect(subject.total).to eq(0.00)
+      end
     end
   end
 end
